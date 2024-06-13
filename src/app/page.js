87c22efaed8c0309e6f8a47cfe1bb7647c99c232
Gabriel Bortolote <gbortolote@gmail.com>
2024-06-13 18:17:29 -0300

    Add skeleton in colors

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,18 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="
-  ">
-    <section>
+  return <>
+    <section className="h-20 bg-light">
       Start Section
     </section>
-    <section>
+    <section className="h-20 bg-darkGreen">
       Specialist Section
     </section>
-    <section>
+    <section className="h-20 bg-lighGreen">
       CV Section
     </section>
-    <section>
+    <section className="h-20 bg-light">
       Contact Section
     </section>
-  </div>
+  </>
 }
