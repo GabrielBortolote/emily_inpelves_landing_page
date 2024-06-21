454fdbb19741d4518bd3e5b8a997c1c36213c1d4
Gabriel Bortolote <gbortolote@gmail.com>
2024-06-21 11:06:41 -0300

    Remove not ready sections

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,5 @@ import StartSection from "@/components/sections/start/StartSection";
 export default function Home() {
   return <>
     <StartSection />
-    <section className="h-20 bg-darkGreen">
-      Specialist Section
-    </section>
-    <section className="h-20 bg-lighGreen">
-      CV Section
-    </section>
-    <section className="h-20 bg-light">
-      Contact Section
-    </section>
   </>
 }
